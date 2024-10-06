3d2c9dc2aaab9bbd500b1d166c5acb7f1f2181b8
yared bekuru <69724291+yaredybk@users.noreply.github.com>
2024-10-06 20:07:49 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,43 @@
+require('dotenv').config();
+const { createClient } = require('redis');
+
+// class for redis commands
+class RedisClient {
+  constructor() {
+    this.isAlive = false;
+    this.cl = createClient(process.env.REDIS_URL);
+    this.cl.connect().catch((error) => {
+      console.log(`Redis client not connected to server: ${error}`);
+    });
+    this.cl.on('connect', (err) => {
+      this.isAlive = true;
+    });
+    this.cl.on('error', (err) => {
+      this.isAlive = false;
+      console.warn('Redis Client Error', err);
+    });
+  }
+
+  // get value for given key
+  async get(key) {
+    const value = await this.cl.get(key);
+    return value;
+  }
+
+  // set key value pair
+  async set(key, value, time) {
+    await this.cl.set(key, value, { EX: time });
+  }
+
+  // del key vale pair
+  async del(key) {
+    await this.cl.del(key);
+  }
+}
+
+const cli = new RedisClient();
+(async ()=>{
+  await cli.set("test", "test passed");
+  const d = await cli.get("test");
+  console.log(d);
+})()
